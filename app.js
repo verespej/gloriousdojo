0928f37464eff6ac7acda0929bdab49e9aa4d022
Hakon <mrhakon@gmail.com>
2014-02-27 17:02:13 -0800

    Add leap motion workshop presentation

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-	res.send('Hi there!');
-});
+app.use(express.directory(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 var port = Number(process.env.PORT ||	5000);
 app.listen(port, function() {
